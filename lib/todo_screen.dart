deee4a30448b445f7327c09501e6211abe127786
chanyoung <chch_@naver.com>
2020-03-12 15:47:33 +0900

    기본 구조 잡음

---
+++ lib/todo_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class TodoScreen extends StatefulWidget {
+  @override
+  _TodoScreenState createState() => _TodoScreenState();
+}
+
+class _TodoScreenState extends State<TodoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'my todo list',
+          style: TextStyle(color: Colors.grey[900]),
+        ),
+        // Only widgets that implement [PreferredSizeWidget]
+        // can be used at the bottom of an app bar
+        bottom: PreferredSize(
+          child: AppBarBottomView(),
+          preferredSize: Size.fromHeight(100),
+        ),
+        backgroundColor: Colors.white.withOpacity(0.95),
+        centerTitle: true,
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () {},
+            child: Text('편집', style: TextStyle(fontSize: 18)),
+          ),
+        ],
+      ),
+      body: TodoList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('click add button');
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.grey[900],
+        ),
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
+
+class AppBarBottomView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: Text(
+          '2020-03-12',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
+
+// # ListTile.divideTiles vs ListView.separated
+// 참고 url:
+// https://stackoverflow.com/questions/50687633/flutter-divider-how-could-i-add-divider-between-each-line-in-my-code
+// https://stackoverflow.com/questions/52207612/how-to-use-dividetiles-in-flutter
+// - short static list: divideTiles, long dynamic list: separated를 사용
+//  -> 이유: 소스코드를 보면 아래와 같이 설명 되어있음
+//    * large number of item and separator children
+//    * because the builders are only called for the children that are actually visible.
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text('$index sheep'),
+          trailing: Icon(Icons.change_history),
+        );
+      },
+      separatorBuilder: (_, index) {
+        return Divider();
+      },
+      itemCount: 5,
+    );
+  }
+}
