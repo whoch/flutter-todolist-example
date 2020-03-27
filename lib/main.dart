df547ea0f99d6159c8623f2baf7e8a33253c0baf
macmini <macmini@macminis-Mac-mini.local>
2020-03-27 10:04:41 +0900

    edit App Icon, name, splash screen(flutter Widget)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,35 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:todolist/todo_screen.dart';
 
 void main() => runApp(MyApp());
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  Future start() async {
+    return Timer(Duration(seconds: 2), navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/home');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    start();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('assets/ghost.png');
+  }
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -12,7 +40,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: TodoScreen(),
+      home: SplashScreen(),
+      routes: <String, WidgetBuilder>{'/home': (_) => TodoScreen()},
     );
   }
 }
